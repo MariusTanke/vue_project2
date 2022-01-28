58ee84058a2bf56cf88cf9eef9bdc7357756d965
Marius Tankelevicius <tankee265@gmail.com>
2022-01-28 17:35:42 +0100

    segundo commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
 
   <div id="app" class="container">
+
     <div class="row">
+
       <div class="col-md-12">
+
         <h1>Personas</h1>
+
       </div>
+
     </div>
 
     <div class="row">
 
+      <div class="col-md-12" style="margin-top: 20px">
+        <formulario @add-persona="agregarPersona" />
+      </div>
+
       <div class="col-md-12">
-        <tabla-personas v-bind:personas="personas" />
+        <tabla-personas
+          :personas="personas"
+          @delete-persona="eliminarPersona"
+          @actualizar-persona="actualizarPersona"
+        />
       </div>
 
       <div class="col-md-12">
         <cards-personas v-bind:personas="personas" />
       </div>
 
-      <div class="col-md-12" style="margin-top: 20px">
-        <formulario />
-      </div>
     </div>
 
   </div>
+
 </template>
 
 <script>
@@ -38,42 +49,69 @@ export default {
       CardsPersonas,
       formulario,
     },
+
+    methods: {
+
+      agregarPersona(persona) {
+
+        let id = 0;
+    
+        if (this.personas.length > 0) {
+
+          id = this.personas[this.personas.length - 1].id + 1;
+
+        }
+        
+        this.personas= [...this.personas, { ...persona, id}];
+
+      },
+
+      eliminarPersona(id) {
+          this.personas = this.personas.filter(
+            persona => persona.id !== id
+          );
+        }
+      },
+
+      actualizarPersona(id, personaActualizada) {
+          this.personas = this.personas.map(persona =>
+            persona.id === id ? personaActualizada : persona
+          )
+        },
+
     data() {
       return {
         personas: [
-          {
-            id: 1,
-            nombre: 'Jon',
-            apellido: 'Nieve',
-            email: 'jon@email.com',
-          },
-          {
-            id: 2,
-            nombre: 'Tyrion',
-            apellido: 'Lannister',
-            email: 'tyrion@email.com',
-          },
-          {
-            id: 3,
-            nombre: 'Daenerys',
-            apellido: 'Targaryen',
-            email: 'daenerys@email.com',
-          },
-          {
-            id: 4,
-            nombre: 'Marius',
-            apellido: 'Tankelevicius',
-            email: 'tankee265@gmail.com',
-          },
-        ],
-      }
-    },
-  }
+            {
+              id: 1,
+              nombre: 'Jon',
+              apellido: 'Nieve',
+              email: 'jon@email.com',
+            },
+            {
+              id: 2,
+              nombre: 'Tyrion',
+              apellido: 'Lannister',
+              email: 'tyrion@email.com',
+            },
+            {
+              id: 3,
+              nombre: 'Daenerys',
+              apellido: 'Targaryen',
+              email: 'daenerys@email.com',
+            },
+          ],
+        }
+      },
+    }
+
 </script>
 
 <style>
+
   button {
     background: #009435;
     border: 1px solid #009435;
   }
+  
 </style>
--- src/components/CardsPersonas.vue
+++ src/components/CardsPersonas.vue
@@ -1,14 +1,22 @@
 <template>
 
-    <div class="card-deck">
-          
-        <div class="card" style="width: 18rem;"  v-for="persona in personas" :key="persona.id">
+    <div>
 
-            <div class="card-body">
-                <h5 class="card-title text">ID: {{ persona.id}}</h5>
-                <p class="card-text">Nombre: {{ persona.nombre}}</p>
-                <p class="card-text">Apellido: {{ persona.apellido}}</p>
-                <p class="card-text">Email: {{ persona.email}}</p>
+        <div class="row">
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="persona in personas" :key="persona.id">
+
+                <div class="card" style="border: solid 3px lightblue">
+
+                    <div class="card-body">
+                        <h5 class="card-title text">ID: {{ persona.id}}</h5>
+                        <p class="card-text">Nombre: {{ persona.nombre}}</p>
+                        <p class="card-text">Apellido: {{ persona.apellido}}</p>
+                        <p class="card-text">Email: {{ persona.email}}</p>
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
